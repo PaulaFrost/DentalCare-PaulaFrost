533db1fc120c771f29a95be7fd17d18efa9db020
Paula Hernandez <paula.frost@hotmail.com>
2018-10-25 10:42:07 +0200

    Add project files.

---
+++ DentalCare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DentalCare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
